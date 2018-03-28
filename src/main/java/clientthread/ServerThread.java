51810e936c6a7ec8aa5b727809937645d175c3e1
王家东 <wb-wjd357062@alibaba-inc.com>
2018-03-28 17:37:58 +0800

    add java class

---
+++ src/main/java/clientthread/ServerThread.java
@@ -0,0 +1,21 @@
+package clientthread;
+
+/**
+ * @author wang
+ * @data on 2018/3/28
+ */
+public class ServerThread implements Runnable{
+
+
+  public void run() {
+
+
+  }
+
+
+  public synchronized  void getMessage(){
+
+
+
+  }
+}
